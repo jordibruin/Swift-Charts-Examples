62af7931193e5beb72307243b31f62f88446a710
Daniel Tavares <vibrazy@hotmail.com>
2022-06-14 16:24:10 +0100

    Adding Daily Health Data - BPM

--- Swift Charts Examples/Data/Data.swift
+++ Swift Charts Examples/Data/Data.swift
@@ -4,8 +4,8 @@
 
 import Foundation
 
-func date(year: Int, month: Int, day: Int = 1) -> Date {
-    Calendar.current.date(from: DateComponents(year: year, month: month, day: day)) ?? Date()
+func date(year: Int, month: Int, day: Int = 1, hour: Int = 0, minutes: Int = 0, seconds: Int = 0) -> Date {
+    Calendar.current.date(from: DateComponents(year: year, month: month, day: day, hour: hour, minute: minutes, second: seconds)) ?? Date()
 }
 
 enum Constants {
