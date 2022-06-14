c5315a7cfb3f4f7d5e0ed4cda118a3f88114f7e6
Darius <32181971+dariusngo@users.noreply.github.com>
2022-06-13 19:43:23 -0500

    Adds Heart Rate data Added picker to select Range Chart Data

--- Swift Charts Examples/Data/Data.swift
+++ Swift Charts Examples/Data/Data.swift
@@ -223,3 +223,57 @@ struct DataUsageData {
         
     ]
 }
+
+/// MARK: - Heart Rate data
+enum HeartRateData {
+    /// Heart Rate for the last week
+    static let lastWeek = [
+        (weekday: date(year: 2022, month: 7, day: 1), dailyAverage: 127, dailyMin: 95, dailyMax: 194),
+        (weekday: date(year: 2022, month: 7, day: 1), dailyAverage: 130, dailyMin: 200, dailyMax: 239),
+        
+        (weekday: date(year: 2022, month: 7, day: 2), dailyAverage: 131, dailyMin: 101, dailyMax: 184),
+        
+        (weekday: date(year: 2022, month: 7, day: 3), dailyAverage: 136, dailyMin: 96, dailyMax: 193),
+        (weekday: date(year: 2022, month: 7, day: 3), dailyAverage: 136, dailyMin: 80, dailyMax: 93),
+        
+        (weekday: date(year: 2022, month: 7, day: 4), dailyAverage: 134, dailyMin: 104, dailyMax: 202),
+        
+        (weekday: date(year: 2022, month: 7, day: 5), dailyAverage: 129, dailyMin: 90, dailyMax: 95),
+        (weekday: date(year: 2022, month: 7, day: 5), dailyAverage: 129, dailyMin: 96, dailyMax: 190),
+        
+        (weekday: date(year: 2022, month: 7, day: 6), dailyAverage: 136, dailyMin: 96, dailyMax: 203),
+        
+        (weekday: date(year: 2022, month: 7, day: 7), dailyAverage: 134, dailyMin: 98, dailyMax: 200),
+    ]
+
+    // MARK: - Static constants
+    
+    static let minBPM: Int = {
+        Self.lastWeek.min { a, b in
+            a.dailyMin < b.dailyMin
+        }?.dailyMin ?? 0
+    }()
+    
+    static let maxBPM: Int = {
+        Self.lastWeek.max { a, b in
+            a.dailyMax < b.dailyMax
+        }?.dailyMax ?? 0
+    }()
+    
+    static let earliestDate: Date = {
+        Self.lastWeek.min { a, b in
+            a.weekday < b.weekday
+        }?.weekday ?? Date()
+    }()
+    
+    static let latestDate: Date = {
+        Self.lastWeek.max { a, b in
+            a.weekday < b.weekday
+        }?.weekday ?? Date()
+    }()
+    
+    static let dateInterval: DateInterval = {
+        DateInterval(start: earliestDate, end: latestDate)
+    }()
+}
+
