cfd8b72db244a4414d65e1cee9e5e03a4241f29d
An Trinh <atrinh@gmail.com>
2022-07-02 12:35:37 +0100

    Address todo comment by auto indenting file with default Xcode preferences in place

--- Swift Charts Examples/Charts/LineCharts/GradientLine.swift
+++ Swift Charts Examples/Charts/LineCharts/GradientLine.swift
@@ -5,15 +5,13 @@
 import SwiftUI
 import Charts
 
-// TODO: Convert to spaces after review. This file uses tabs while the rest of this project uses space.
-
 struct GradientLine: View {
-	var isOverview: Bool
+    var isOverview: Bool
 
-	@State private var selectedDate: Date?
+    @State private var selectedDate: Date?
     @State var data = WeatherData.hourlyUVIndex
 
-	var body: some View {
+    var body: some View {
         Group {
             if isOverview {
                 chart
@@ -28,126 +26,126 @@ struct GradientLine: View {
             }
         }
         .accessibilityChartDescriptor(self)
-	}
+    }
 
-	private var chart: some View {
-		Chart {
-			RectangleMark(
-				xStart: .value("hour", Calendar.current.startOfDay(for: Date())),
-				xEnd: .value("hour", Calendar.current.startOfDay(for: Date()).addingTimeInterval(60*60*23))
-			)
-			.foregroundStyle(.linearGradient(stops: [
-				Gradient.Stop(color: .green, location: 0),
-				Gradient.Stop(color: .green, location: 2/14),
-				Gradient.Stop(color: .yellow, location: 5/14),
-				Gradient.Stop(color: .orange, location: 8/14),
-				Gradient.Stop(color: .red, location: 10/14),
-				Gradient.Stop(color: .purple, location: 14/14),
-			], startPoint: .bottom, endPoint: .top))
-			.mask {
-				if let max = WeatherData.hourlyUVIndex.max(by: { $0.uvIndex < $1.uvIndex }) {
-					ForEach(WeatherData.hourlyUVIndex, id: \.date) { hour in
-						AreaMark(
-							x: .value("hour", hour.date),
-							y: .value("uvIndex", hour.uvIndex)
-						)
-						.interpolationMethod(.cardinal)
-						.foregroundStyle(.black.opacity(0.4))
+    private var chart: some View {
+        Chart {
+            RectangleMark(
+                xStart: .value("hour", Calendar.current.startOfDay(for: Date())),
+                xEnd: .value("hour", Calendar.current.startOfDay(for: Date()).addingTimeInterval(60*60*23))
+            )
+            .foregroundStyle(.linearGradient(stops: [
+                Gradient.Stop(color: .green, location: 0),
+                Gradient.Stop(color: .green, location: 2/14),
+                Gradient.Stop(color: .yellow, location: 5/14),
+                Gradient.Stop(color: .orange, location: 8/14),
+                Gradient.Stop(color: .red, location: 10/14),
+                Gradient.Stop(color: .purple, location: 14/14),
+            ], startPoint: .bottom, endPoint: .top))
+            .mask {
+                if let max = WeatherData.hourlyUVIndex.max(by: { $0.uvIndex < $1.uvIndex }) {
+                    ForEach(WeatherData.hourlyUVIndex, id: \.date) { hour in
+                        AreaMark(
+                            x: .value("hour", hour.date),
+                            y: .value("uvIndex", hour.uvIndex)
+                        )
+                        .interpolationMethod(.cardinal)
+                        .foregroundStyle(.black.opacity(0.4))
 
-						LineMark(
-							x: .value("hour", hour.date),
-							y: .value("uvIndex", hour.uvIndex)
-						)
-						.interpolationMethod(.cardinal)
-						.lineStyle(StrokeStyle(lineWidth: 4))
-						.symbol(Circle().strokeBorder(style: StrokeStyle(lineWidth: 0)))
-						.symbolSize(hour.date == max.date ? CGSize(width: 10, height: 10) : .zero)
-					}
+                        LineMark(
+                            x: .value("hour", hour.date),
+                            y: .value("uvIndex", hour.uvIndex)
+                        )
+                        .interpolationMethod(.cardinal)
+                        .lineStyle(StrokeStyle(lineWidth: 4))
+                        .symbol(Circle().strokeBorder(style: StrokeStyle(lineWidth: 0)))
+                        .symbolSize(hour.date == max.date ? CGSize(width: 10, height: 10) : .zero)
+                    }
 
-					PointMark(
-						x: .value("hour", max.date),
-						y: .value("uvIndex", max.uvIndex)
-					)
-					.symbolSize(CGSize(width: 5, height: 5))
-					.foregroundStyle(.green)
-					.annotation(spacing: 0) {
-						Text("\(max.uvIndex)")
-							.font(.caption.weight(.bold))
-							.foregroundColor(.secondary)
-					}
-				}
-			}
+                    PointMark(
+                        x: .value("hour", max.date),
+                        y: .value("uvIndex", max.uvIndex)
+                    )
+                    .symbolSize(CGSize(width: 5, height: 5))
+                    .foregroundStyle(.green)
+                    .annotation(spacing: 0) {
+                        Text("\(max.uvIndex)")
+                            .font(.caption.weight(.bold))
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
 
-			if let selectedDate, let uvIndex = WeatherData.hourlyUVIndex.first(where: { $0.date == selectedDate })?.uvIndex {
-				RuleMark(x: .value("hour", selectedDate))
-					.foregroundStyle(Color(.label))
-				PointMark(
-					x: .value("hour", selectedDate),
-					y: .value("uvIndex", uvIndex)
-				)
-				.symbolSize(CGSize(width: 15, height: 15))
-				.foregroundStyle(Color(.label))
-			}
-		}
-		.chartYScale(domain: 0...14)
-		.chartYAxis {
-			AxisMarks(position: .trailing, values: .automatic(desiredCount: 14)) { axisValue in
-				if axisValue.index % 2 == 0 {
-					AxisValueLabel()
-				}
-				AxisGridLine()
-			}
+            if let selectedDate, let uvIndex = WeatherData.hourlyUVIndex.first(where: { $0.date == selectedDate })?.uvIndex {
+                RuleMark(x: .value("hour", selectedDate))
+                    .foregroundStyle(Color(.label))
+                PointMark(
+                    x: .value("hour", selectedDate),
+                    y: .value("uvIndex", uvIndex)
+                )
+                .symbolSize(CGSize(width: 15, height: 15))
+                .foregroundStyle(Color(.label))
+            }
+        }
+        .chartYScale(domain: 0...14)
+        .chartYAxis {
+            AxisMarks(position: .trailing, values: .automatic(desiredCount: 14)) { axisValue in
+                if axisValue.index % 2 == 0 {
+                    AxisValueLabel()
+                }
+                AxisGridLine()
+            }
 
-			AxisMarks(preset: .inset, position: .leading, values: .automatic(desiredCount: 14)) { axisValue in
-				switch axisValue.index {
-				case 1:
-					AxisValueLabel("Low", anchor: .topLeading)
-				case 3:
-					AxisValueLabel("Moderate", anchor: .topLeading)
-				case 6:
-					AxisValueLabel("High", anchor: .topLeading)
-				case 8:
-					AxisValueLabel("Very high", anchor: .topLeading)
-				case 11:
-					AxisValueLabel("Extreme", anchor: .topLeading)
-				default:
-					EmptyAxisMark()
-				}
-			}
-		}
-		.chartXAxis {
-			AxisMarks(position: .bottom, values: .automatic) { _ in
-				AxisValueLabel()
-				AxisGridLine()
-				AxisTick()
-			}
+            AxisMarks(preset: .inset, position: .leading, values: .automatic(desiredCount: 14)) { axisValue in
+                switch axisValue.index {
+                case 1:
+                    AxisValueLabel("Low", anchor: .topLeading)
+                case 3:
+                    AxisValueLabel("Moderate", anchor: .topLeading)
+                case 6:
+                    AxisValueLabel("High", anchor: .topLeading)
+                case 8:
+                    AxisValueLabel("Very high", anchor: .topLeading)
+                case 11:
+                    AxisValueLabel("Extreme", anchor: .topLeading)
+                default:
+                    EmptyAxisMark()
+                }
+            }
+        }
+        .chartXAxis {
+            AxisMarks(position: .bottom, values: .automatic) { _ in
+                AxisValueLabel()
+                AxisGridLine()
+                AxisTick()
+            }
 
-			AxisMarks(position: .top, values: .automatic(desiredCount: 24)) { value in
-				if value.index % 2 != 0 {
-					AxisValueLabel("\(WeatherData.hourlyUVIndex[value.index].uvIndex)", anchor: .bottom)
-				}
-			}
-		}
-		.chartOverlay { proxy in
-			GeometryReader { g in
-				Rectangle().fill(.clear).contentShape(Rectangle())
-					.gesture(
-						DragGesture(minimumDistance: 0)
-							.onChanged { value in
-								let x = value.location.x - g[proxy.plotAreaFrame].origin.x
-								if let date: Date = proxy.value(atX: x), let roundedHour = date.nearestHour() {
-									self.selectedDate = roundedHour
-								}
-							}
-							.onEnded { value in
-								self.selectedDate = nil
-							}
-					)
-			}
-		}
-		.chartYAxis(isOverview ? .hidden : .visible)
-		.chartXAxis(isOverview ? .hidden : .visible)
-		.frame(height: isOverview ? Constants.previewChartHeight : Constants.detailChartHeight)
+            AxisMarks(position: .top, values: .automatic(desiredCount: 24)) { value in
+                if value.index % 2 != 0 {
+                    AxisValueLabel("\(WeatherData.hourlyUVIndex[value.index].uvIndex)", anchor: .bottom)
+                }
+            }
+        }
+        .chartOverlay { proxy in
+            GeometryReader { g in
+                Rectangle().fill(.clear).contentShape(Rectangle())
+                    .gesture(
+                        DragGesture(minimumDistance: 0)
+                            .onChanged { value in
+                                let x = value.location.x - g[proxy.plotAreaFrame].origin.x
+                                if let date: Date = proxy.value(atX: x), let roundedHour = date.nearestHour() {
+                                    self.selectedDate = roundedHour
+                                }
+                            }
+                            .onEnded { value in
+                                self.selectedDate = nil
+                            }
+                    )
+            }
+        }
+        .chartYAxis(isOverview ? .hidden : .visible)
+        .chartXAxis(isOverview ? .hidden : .visible)
+        .frame(height: isOverview ? Constants.previewChartHeight : Constants.detailChartHeight)
         .accessibilityRepresentation {
             Chart(data, id: \.date) { hour in
                 Plot {
@@ -161,7 +159,7 @@ struct GradientLine: View {
             }
             .accessibilityHidden(isOverview)
         }
-	}
+    }
 }
 
 // MARK: - Accessibility
@@ -209,8 +207,8 @@ extension GradientLine: AXChartDescriptorRepresentable {
 // MARK: - Preview
 
 struct GradientLine_Previews: PreviewProvider {
-	static var previews: some View {
+    static var previews: some View {
         GradientLine(isOverview: true)
         GradientLine(isOverview: false)
-	}
+    }
 }
