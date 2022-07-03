18e0f5e6841ff2968aaff94c88adf3d58351c6c7
An Trinh <atrinh@gmail.com>
2022-07-03 22:13:44 +0100

    WIP converting heartbeat to sinus rhythm chart

--- Swift Charts Examples/Charts/LineCharts/HeartBeat.swift
+++ Swift Charts Examples/Charts/LineCharts/HeartBeat.swift
@@ -6,68 +6,81 @@ import SwiftUI
 import Charts
 
 struct HeartBeat: View {
-	var isOverview: Bool
+    var isOverview: Bool
 
-    @State var data = HealthData.ecgSample
+    @State private var data = HealthData.ecgSample
     @State private var lineWidth = 2.0
     @State private var interpolationMethod: ChartInterpolationMethod = .cardinal
     @State private var chartColor: Color = .pink
     
     var body: some View {
-		if isOverview {
-			chart
-				.allowsHitTesting(false)
-		} else {
-			List {
-				Section {
-					chart
-				}
+        if isOverview {
+            chart
+                .allowsHitTesting(false)
+        } else {
+            List {
+                Section {
+                    chart
+                }
 
-				customisation
-			}
-			.navigationBarTitle(ChartType.heartBeat.title, displayMode: .inline)
-		}
+                customisation
+            }
+            .navigationBarTitle(ChartType.heartBeat.title, displayMode: .inline)
+        }
     }
 
-	private var chart: some View {
-		Chart {
-			ForEach(Array(data.enumerated()), id: \.element) { index, element in
-				LineMark(
-					x: .value("Index", index),
-					y: .value("Unit", element)
-				)
-				.lineStyle(StrokeStyle(lineWidth: lineWidth))
-				.foregroundStyle(chartColor.gradient)
-				.interpolationMethod(interpolationMethod.mode)
-				.accessibilityLabel("\(index)s")
-				.accessibilityValue("\(element) mV")
+    private var chart: some View {
+//        VStack {
+//            Text("Sinus Rhythum")
+//                .font(.largeTitle)
+//        }
+        Chart {
+            ForEach(Array(data.enumerated()), id: \.element) { index, element in
+                LineMark(
+                    x: .value("Seconds", Double(index)/400.0),
+                    y: .value("Unit", element)
+                )
+                .lineStyle(StrokeStyle(lineWidth: lineWidth))
+                .foregroundStyle(chartColor)
+                .interpolationMethod(interpolationMethod.mode)
+                .accessibilityLabel("\(index)s")
+                .accessibilityValue("\(element) mV")
                 .accessibilityHidden(isOverview)
-			}
-		}
-		.chartYAxis {
-			AxisMarks(preset: .aligned, position: .automatic) { value in
-				let rawValue = value.as(Double.self)! / 100
-				let formattedValue = rawValue.formatted()
-
-				AxisGridLine()
-				AxisValueLabel(formattedValue)
-			}
-		}
-		.chartXAxis {
-			AxisMarks(preset: .aligned, position: .automatic) { value in
-				let rawValue = value.as(Int.self)! / 100
-				let formattedValue = rawValue.formatted()
-
-				AxisGridLine()
-				AxisValueLabel(formattedValue)
-			}
-		}
+            }
+        }
+        .chartXAxis {
+            AxisMarks(values: .automatic(desiredCount: 12)) { value in
+                if let doubleValue = value.as(Double.self),
+                   let intValue = value.as(Int.self) {
+                    if doubleValue - Double(intValue) == 0 {
+                        AxisTick(stroke: .init(lineWidth: 1))
+                            .foregroundStyle(.gray)
+                        AxisValueLabel() {
+                            Text("\(intValue)s")
+                        }
+                        AxisGridLine(stroke: .init(lineWidth: 1))
+                            .foregroundStyle(.gray)
+                    } else {
+                        AxisGridLine(stroke: .init(lineWidth: 1))
+                            .foregroundStyle(.gray.opacity(0.25))
+                    }
+                }
+            }
+        }
+        .chartYScale(domain: -400...800)
+        .chartYAxis {
+            AxisMarks(values: .automatic(desiredCount: 14)) { value in
+                AxisGridLine(stroke: .init(lineWidth: 1))
+                    .foregroundStyle(.gray.opacity(0.25))
+            }
+        }
+        .chartPlotStyle {
+            $0.border(Color.gray, width: 1)
+        }
         .accessibilityChartDescriptor(self)
-		.chartYAxis(isOverview ? .hidden : .automatic)
-		.chartXAxis(isOverview ? .hidden : .automatic)
-		.frame(height: isOverview ? Constants.previewChartHeight : Constants.detailChartHeight)
-	}
-    
+        .frame(height: Constants.detailChartHeight)
+    }
+
     private var customisation: some View {
         Section {
             VStack(alignment: .leading) {
@@ -80,11 +93,11 @@ struct HeartBeat: View {
                     Text("10")
                 }
             }
-            
+
             Picker("Interpolation Method", selection: $interpolationMethod) {
                 ForEach(ChartInterpolationMethod.allCases) { Text($0.mode.description).tag($0) }
             }
-            
+
             ColorPicker("Color Picker", selection: $chartColor)
         }
     }
@@ -136,7 +149,7 @@ extension HeartBeat: AXChartDescriptorRepresentable {
 struct HeartBeat_Previews: PreviewProvider {
     static var previews: some View {
         HeartBeat(isOverview: true)
-		HeartBeat(isOverview: false)
+        HeartBeat(isOverview: false)
     }
 }
 
